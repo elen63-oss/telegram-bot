cc125539ff8828e4297371d41300f9f6ed52051d
elen63-oss <gribanovme@gmail.com>
2025-05-28 00:57:27 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
