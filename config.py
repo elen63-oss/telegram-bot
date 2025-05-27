9dc758868618b648014a7e8e7838a513dcb88ec6
elen63-oss <gribanovme@gmail.com>
2025-05-28 01:49:04 +0300

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-cat > /root/telegram-bot/config.py <<EOL
 BOT_TOKEN = "7205523596:AAHXkEyjS_9DZl3cIx5qFku4tdcP3muc2-E"
 ADMIN_ID = 1493002303  # только цифры
 GROUP_LINK = "t.me/elen63investchannel"  # или @username
 MAX_PARTICIPANTS = 1500  # нужное число участников
-EOL
