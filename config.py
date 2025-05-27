1af3854041a92227e01c33bbd0a40a55746098d1
elen63-oss <gribanovme@gmail.com>
2025-05-28 02:21:44 +0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "7205523596:AAHXkEyjS_9DZl3cIx5qFku4tdcP3muc2-E"
 ADMIN_ID = 1493002303  # только цифры
-GROUP_LINK = "t.me/elen63investchannel"  # или @username
+GROUP_LINK = "https://t.me/elen63investchannel"  # или @username
 MAX_PARTICIPANTS = 1500  # нужное число участников
