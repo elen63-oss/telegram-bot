8ef995cdedae9e6c20bd0bc705422bd4f8858b99
elen63-oss <gribanovme@gmail.com>
2025-05-28 01:32:00 +0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-BOT_TOKEN = "7205523596:AAHXkEyjS_9DZl3cIx5qFku4tdcP3muc2-E"  # Замените на реальный токен
-ADMIN_ID = 1493002303                                         # Ваш ID в Telegram
-GROUP_LINK = "t.me/elen63investchannel"                       # Ссылка на канал
-MAX_PARTICIPANTS = 1500                                       # Лимит участников
+cat > /root/telegram-bot/config.py <<EOL
+BOT_TOKEN = "7205523596:AAHXkEyjS_9DZl3cIx5qFku4tdcP3muc2-E"
+ADMIN_ID = 1493002303  # только цифры
+GROUP_LINK = "t.me/elen63investchannel"  # или @username
+MAX_PARTICIPANTS = 1500  # нужное число участников
+EOL
